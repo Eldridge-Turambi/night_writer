726e3ff95434883ee00bf33b7627df585c7276ce
Eldridge Turambi <eturambi95@gmail.com>
2021-09-26 13:36:41 -0600

    Add feautures to connect message.txt => braille.txt

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,14 +1,26 @@
 class NightWriter
 
-  #def initialize(file_path)
+  def initialize
+    @input_file = ARGV[0]
+    @output_file = ARGV[1]
+  end
 
 
-  #end
+  def read_message
+    File.open(@input_file).read
+  end
 
-  def print_message
-    file = File.open("message.txt").read
-    puts file
+  def convert_text
+    read_message
+  end
+
+  def read_write
+    message = convert_text
+    File.open(@output_file, 'w') do |file|
+      file.write(message)
+    end
   end
 end
 
-NightWriter.new.print_message
+# NightWriter.new.print_message
+ NightWriter.new.read_write
