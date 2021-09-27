1b720c917da30569b5582ceee1a701dc8c82c63a
Eldridge Turambi <eturambi95@gmail.com>
2021-09-26 20:32:04 -0600

    Add convert_english_message method. Works :)

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -6,6 +6,11 @@ class NightWriter
     @input_file = ARGV[0]
     @output_file = ARGV[1]
     @bd = BrailleDictionary.new.braille_dictionary
+    @braille_dictionary = {
+      'a' => ['0.', '..', '..'],
+      'b' => ['0.', '0.', '..'],
+      'c' => ['00', '..', '..']
+    }
   end
 
 
@@ -15,10 +20,13 @@ class NightWriter
 
   def convert_english_message
     english_letters = read_message.chars
+    braille_letters = []
     english_letters.each do |letter|
-      letter.find_by_letter()
-
+      x = find_by_letter(letter)
+      braille_letters << x
     end
+    braille_letters.compact.transpose
+
   end
 
   def read_write
@@ -26,6 +34,10 @@ class NightWriter
     File.open(@output_file, 'w') do |file|
       file.write(message)
     end
+  end
+
+  def find_by_letter(single_letter)
+    braille_code = @braille_dictionary[single_letter]
   end
 end
 
