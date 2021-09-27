2a8257a35b5aff2f72a9063301e7195d7433486f
Eldridge Turambi <eturambi95@gmail.com>
2021-09-27 09:50:34 -0600

    Created BrailleDicitonary class with transfered english to braille method

--- lib/braille_dictionary.rb
+++ lib/braille_dictionary.rb
@@ -2,13 +2,16 @@ class BrailleDictionary
   attr_reader :braille_dictionary
 
   def initialize
-    # @braille_dictionary = {
-    #   'a' => ['0.', '..', '..'],
-    #   'b' => ['0.', '0.', '..'],
-    #   'c' => ['00', '..', '..']
-    # }
+    @braille_dictionary = {
+      'a' => ['0.', '..', '..'],
+      'b' => ['0.', '0.', '..'],
+      'c' => ['00', '..', '..']
+    }
   end
 
+  def translate_letter_to_braille(single_letter)
+    @braille_dictionary[single_letter]
+  end
   # def find_by_letter(single_letter)
   #   braille_code = @braille_dictionary[single_letter]
   #   puts braille_code
