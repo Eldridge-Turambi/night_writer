7aafe88b8ccc00014752f3fe7b6757743126bd83
Eldridge Turambi <eturambi95@gmail.com>
2021-09-27 14:07:14 -0600

    Add JSON method to convert string of nested array into soley a nested array

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,10 +1,12 @@
 require_relative 'english_dictionary'
+require 'json'
 
 class NightReader
 
   def initialize
     @input_file = ARGV[0]
     @output_file = ARGV[1]
+    @english_dictionary_1 = EnglishDictionary.new
   end
 
   def read_braille_message
@@ -12,11 +14,17 @@ class NightReader
   end
 
   def convert_braille_message
-    
+    braille_letters = JSON.parse( read_braille_message.tr("'", '"') )
+    english_letters = []
+
+    braille_letters.each do |letter|
+      english_letters << @english_dictionary_1.translate_braille_to_letter(letter)
+    end
+    return english_letters.join
   end
 
   def read_write
-    english_string = read_braille_message
+    english_string = convert_braille_message
     File.open(@output_file, 'w') do |file|
       file.write(english_string)
     end
